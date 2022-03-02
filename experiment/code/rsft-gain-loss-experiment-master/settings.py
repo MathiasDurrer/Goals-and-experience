41954b94f29414fb276ba49c62577b1f78f86df7
MathiasDurrer <m.durrer@stud.unibas.ch>
2022-03-02 20:16:33 +0100

    Update settings.py

--- experiment/code/rsft-gain-loss-experiment-master/settings.py
+++ experiment/code/rsft-gain-loss-experiment-master/settings.py
@@ -92,8 +92,8 @@ ADMIN_USERNAME = 'admin'
 # for security, best to set admin password in an environment variable
 ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
 
-AWS_ACCESS_KEY_ID = environ.get()
-AWS_SECRET_ACCESS_KEY = environ.get()
+AWS_ACCESS_KEY_ID = environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = environ.get('AWS_SECRET_ACCESS_KEY')
 
 DEMO_PAGE_INTRO_HTML = """
 Here are some oTree games.
